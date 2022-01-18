c85ab29296107274cb7ba1c5369013c006738e68
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2022-01-18 14:23:07 +0200

    third

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
-
+//import'./css/styles.css'
 const inputForm = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
 const ladMoreBtn = document.querySelector('.load-more');
@@ -11,29 +11,30 @@ let pagenumber = 1;
 let gallery;
 ladMoreBtn.style.display = "none";
 
-async function getImages(event) {
+async function getImages(event) {  
   event.preventDefault();
+  ladMoreBtn.style.display = await "none";
   try {
     const response = await axios.get(`https://pixabay.com/api/?key=25260590-534fb4fdfe1550b09a9c38aa6&q=${event.currentTarget.searchQuery.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pagenumber}`);
-    if(response.data.hits.length === 0){
+    if(response.data.hits.length === 0){   
       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      ladMoreBtn.style.display = "none";
     } else {
       const galleryImages = response.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
-    `<a class="gallery__item" href="${largeImageURL}">    
-    <img class = "gallery__image" src = "${webformatURL}" alt="${tags}"/>           
+    `<a class="gallery__item gallery__link" href="${largeImageURL}">    
+    <img class = "gallery__image" src = "${webformatURL}" alt="${tags}"/>
+    <ul>
+    <li class ="disabled-link"><h4>likes</h4><p>${likes}</p></li>
+    <li class ="disabled-link"><h4>views</h4><p>${views}</p></li>
+    <li class ="disabled-link"><h4>comments</h4><p>${comments}</p></li>
+    <li class ="disabled-link"><h4>downloads</h4><p>${downloads}</p></li>
+    </ul>           
       </a>
-      <ul>
-      <li><h4>likes</h4><p>${likes}</p></li>
-      <li><h4>views</h4><p>${views}</p></li>
-      <li><h4>comments</h4><p>${comments}</p></li>
-      <li><h4>downloads</h4><p>${downloads}</p></li>
-      </ul>
       `
       ).join("");
       galleryList.insertAdjacentHTML('beforeend', galleryImages);
       gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt'});
       gallery.on('show.simplelightbox', function () {});
+      ladMoreBtn.style.display = "block";
     }
   } catch (error) {
     console.error(error);
@@ -41,10 +42,11 @@ async function getImages(event) {
 }
 
 inputForm.addEventListener('submit', async (e) => {
-  pagenumber = 1;
+  
   galleryList.innerHTML = "";
-  getImages(e);
-  ladMoreBtn.style.display = "block";
+  pagenumber = 1;
+  await getImages(e);
+
 });
 
 ladMoreBtn.addEventListener('click', async() => {
@@ -53,6 +55,7 @@ ladMoreBtn.addEventListener('click', async() => {
     const newresponse = await axios.get(`https://pixabay.com/api/?key=25260590-534fb4fdfe1550b09a9c38aa6&q=${inputForm.searchQuery.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pagenumber}`);
     if(newresponse.data.hits.length === 0){
       Notiflix.Notify.failure('Sorry, there are no more images matching your search query.');
+      ladMoreBtn.style.display = "none";
     } else {
       const galleryImages = newresponse.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
     `<a class="gallery__item" href="${largeImageURL}">    
